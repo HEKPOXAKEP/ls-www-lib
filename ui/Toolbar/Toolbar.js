8897a5b51c6b081ad7b041e045489a0cea53f93c
Tosh Judeinov <hellish.engineer@gmail.com>
2023-11-22 03:45:44 +0300

    The Toolbar base class is extended: * the bindEvents method attaches a 'click' event handler to all buttons * base method toolbarBtnClick(ev) returns the Id of the button

--- ui/Toolbar/Toolbar.js
+++ ui/Toolbar/Toolbar.js
@@ -6,18 +6,45 @@
   Toolbar.css и ToolbarControl.js.
 
   Загрузка и инициализация производится в классе ToolbarControl
-  методом loadToolbar(id,name,containerId).
+  методом loadToolbar(id,fHtml,fJs,containerId).
+
+  У каждого наследника обязательно перкрыть метод toolBtnClick,
+  в который отсюда возваращается Id. Кнопки должны иметь Id
+  вида toolbtn-XXX, гду XXX и есть Id, который вернёт super(ev).
 */
 class Toolbar {
+
   constructor(containerId) {
     this.containerId=containerId;
+    this.boundToolbarBtnClick=this.toolbarBtnClick.bind(this);
   }
 
   bindEvents() {
-    // abstract
+    var
+      btns=document.querySelectorAll('#'+this.containerId+'>.toolbar-btn');
+
+    Array.from(btns).forEach((btn) => {
+      btn.addEventListener('click',this.boundToolbarBtnClick);
+    });
   }
 
   unbindEvents() {
-    $('#'+this.containerId+'>div').unbind();
+    ///$('#'+this.containerId+'>div').unbind();
+    var
+      btns=document.querySelectorAll('#'+this.containerId+'>.toolbar-btn');
+
+    Array.from(btns).forEach((btn) => {
+      btn.removeEventListener('click',this.boundToolbarBtnClick);
+    });
+  }
+
+  /*
+    super(ev) returns bittons Id
+  */
+  toolbarBtnClick(ev) {
+    if (ev.target.id) return ev.target.id.split('-')[1];
+    else return ev.target.parentNode.id.split('-')[1];
+    /*if (ev.target.id) console.log('clicked ',ev.target.id.split('-')[1]);
+    else console.log(ev.target.parentNode.id.split('-')[1]);*/
   }
 }
